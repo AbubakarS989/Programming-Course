2b7ced0fbfa1b3961a3535634277096828ab28b1
Abubakar Hafeez <138915696+AbubakarS989@users.noreply.github.com>
2023-10-09 09:40:55 +0500

    Scanf

---
+++ scanf.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+ 
+// Example 1:
+//Take integer 
+    int number;
+    printf("Enter a number:");
+    scanf("%d",&number);
+    
+
+
+// Example 2:
+// Take float
+
+    int length=23;
+    printf("The length of pipe is : %d Meter\n",length);
+
+
+// Example 3:
+// Take char
+    int score=10;
+    float height=5.8;
+    char grade='A';
+
+    printf("My height is %f\nMy score is %d\nMy Grade is %c\n",height,score,grade);
+
+
+
+//Example 4:
+// Take string 
+
+
+
+
+
+
+
+}
