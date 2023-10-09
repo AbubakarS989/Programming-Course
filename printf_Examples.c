eec0fc856fad615682c7ce8e1d19c5b67340c809
Abubakar Hafeez <138915696+AbubakarS989@users.noreply.github.com>
2023-10-09 08:46:13 +0500

    PrintF

--- printf_Examples.c
+++ printf_Examples.c
@@ -11,17 +11,23 @@ int main(){
 // Print value
 
     int length=23;
-    printf("The length of pipe is : %dn\n",length);
+    printf("The length of pipe is : %d Meter\n",length);
 
 
-// Example 2:
+// Example 3:
 // Print multiple values
     int score=10;
     float height=5.8;
     char grade='A';
 
     printf("My height is %f\nMy score is %d\nMy Grade is %c\n",height,score,grade);
-    
+    // Output
+    // My height is 5.800000
+    // My score is 10
+    // My Grade is A    
 
+//Note: If we write variables not in the oder of writing format specifier then output is also display in different oder.
+// As I write %f for height and %d for score, if I replace %d and %f, then our output is different .
+// So be conscious while writing.
 
 }
