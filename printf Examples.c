e169f35da6be7593e5f3209f7398524069c94ff8
Abubakar Hafeez <138915696+AbubakarS989@users.noreply.github.com>
2023-10-09 08:39:34 +0500

    Update printf Examples.c

--- printf Examples.c
+++ printf Examples.c
@@ -1,7 +1,27 @@
 #include<stdio.h>
 int main(){
  
-    int number;
-    printf('Enter your number');
-    scanf('%d',&number);
+// Example 1:
+//Simple Print
+
+    printf("Hey! How are you?");
+
+
+// Example 2:
+// Print value
+
+    int height=23;
+    printf("The height of wall: %dn\n",height);
+
+
+// Example 2:
+// Print multiple values
+    int score=10;
+    float height=5.8;
+    char grade='A';
+
+    printf("My height is %f\n My score is %d\n My Grade is %c\n",height,score,grade);
+    
+
+
 }
