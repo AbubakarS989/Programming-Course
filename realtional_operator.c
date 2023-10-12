bbf88c4d72a567c60de48406cb189c9bca54c35d
Abubakar Hafeez <138915696+AbubakarS989@users.noreply.github.com>
2023-10-12 18:35:32 +0500

    Reational Operators

---
+++ realtional_operator.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main(){
+ 
+    int a,b,c;
+    a=10,b=23;
+    c=a<=b;
+    printf("%d",c);
+    // if(a>b)
+    // {
+    //     printf("B is less than A ");
+    // }
+    // else{
+        
+    //     printf("A is less than B ");
+    // }
+
+    // if(a<b)
+    // {
+    //     printf("B is less than A \n");
+    // }
+    // else{
+        
+    //     printf("A is less than B\n ");
+    // }
+
+    // if(a==b){
+    //     printf("A is equal to b");
+    // }
+    // else{
+    //     printf("NO");
+    // }
+    // if(a<=b){
+    //     printf("A is equal to b");
+    // }
+    // else{
+    //     printf("NO");
+    // }
+    if(a<=b){
+        printf("A is equal to b");
+    }
+    else{
+        printf("NO");
+    }
+    
+}
