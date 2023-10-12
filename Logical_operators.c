512c21a8091da0809624a7b0db8700815622e416
Abubakar Hafeez <138915696+AbubakarS989@users.noreply.github.com>
2023-10-12 14:31:28 +0500

    Logical Operaror

---
+++ Logical_operators.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+
+// TODO Write a program that compare two relational expression using AND Operator, to check if both expression result is true or not.
+
+
+int number1,number2;
+
+number1=10;
+number2=20;
+
+if (number1>=10 && number2<20)
+{
+    printf("Both expression is true\n");
+}
+else
+{
+    printf("Both expression is true\n");
+}
+
+}
