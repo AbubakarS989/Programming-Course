22da1970dad29490ed08753ed24dd7b40dfa6ab7
Abubakar Hafeez <138915696+AbubakarS989@users.noreply.github.com>
2023-10-16 19:06:11 +0500

    Precedence

---
+++ operatior_precendence.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+// List of precedence
+
+// Logical NOt !
+// * / %
+// + - 
+// > < >= <=
+// == !=
+//  &&
+//  || 
+//  =  
+
+
+
+float eq;
+
+eq=6+76-56/4-(6*21+1);
+printf("%f",eq);
+
+
+}
