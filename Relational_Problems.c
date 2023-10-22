c3cf25628b865389afc548476cc6c9ad550bd791
Abubakar Hafeez <138915696+AbubakarS989@users.noreply.github.com>
2023-10-22 08:31:38 +0500

    Problems

--- Relational_Problems.c
+++ Relational_Problems.c
@@ -1,5 +1,49 @@
 #include<stdio.h>
 int main(){
  
-    printf("Hi");
+// Relational Operators Problems
+// ! Result will be in 0 and 1
+
+// >
+// <
+// ==
+// !=
+// >=
+// <=
+
+// !P 1 
+int a=10,b=34,c=87;
+int result;
+result=a+b>c;
+printf("%d",result);
+
+// !P 2
+int a=67,b=9,c=37;
+int result;
+
+result=a<b+c;
+printf("%d",result);
+
+// !p 3
+int a=10,b=34,c=87,d=26;
+int result;
+
+result=a/b<=c-d;
+printf("%d",result);
+
+// !p 4
+int a=65,b=41,c=17,d=66;
+int result;
+
+result=a*3+b>=a-d+c;
+printf("%d",result);
+
+// !p 5
+int a=89,b=26,c=13,d=34;
+int result;
+
+result=a%c+34>=b-d+12*c;
+printf("%d",result);
+
+
 }
---
+++ logical_Problems.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main(){
+ 
+// Logical Operators
+
+// &&
+// ||
+//  !
+
+// ! Answer will be in 0 and 1
+
+// ! problem 1
+// int a=23, b=16, c=31,d=54;
+// int result;
+// result=a+b>d && c+a<=b;
+// printf("%d",result) ;
+
+
+// ! problem 2
+// int a=83, b=36, c=21,d=64;
+// int result;
+// result=d-c>a||c*a!=b;
+// printf("%d",result) ;
+
+// ! problem 3
+// int a=13, b=54, c=43,d=14;
+// int result;
+// result=b%d<=d+a && !(a/b>d);
+// printf("%d",result) ;
+
+// ! problem 4
+// int a=63, b=28, c=33,d=65;
+// int result;
+// result=!(a+3%d<a+d*2) ||c+d*a%b<d/11+a-c;
+// printf("%d",result) ;
+
+
+// ! problem 5
+int a=34, b=11, c=21,d=39;
+int result;
+result=b+5%b<=d+a*2 && a+21*b%d>a/c+b-d+34;
+printf("%d",result) ;
+
+}
